b79973a6d0d7c0ae138b3fc106d8f249bdc10960
shivanshu gupta <rajkishoregupta716@gmail.com>
2026-04-06 12:05:41 +0530

    vgju

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -127,7 +127,7 @@ const Footer = () => {
           Cilli<span className="text-blue-500 font-bold">Blog</span>
         </div>
         <div className="text-gray-400 text-sm hidden md:flex">
-          <p>&copy; 2024 DhiWise PVT. LTD. All rights reserved</p>
+          <p>&copy; 2026  All rights reserved</p>
         </div>
         <div className="mt-4 md:mt-0 flex space-x-4">
           <a href="#">
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import MyBlogs from "../dashboard/MyBlogs";
 import CreateBlog from "../dashboard/CreateBlog";
 import UpdateBlog from "../dashboard/UpdateBlog";
 import { Navigate } from "react-router-dom";
+
 function Dashboard() {
   const { profile, isAuthenticated } = useAuth();
   const [component, setComponent] = useState("My Blogs");
